602965b622a1ccbb0ce41ba3b02669e3f8d5d2bd
Aws Ayyash <aws.ayyash.1@gmail.com>
2022-11-16 14:13:22 +0200

    first commit- empty activity

---
+++ app/src/main/java/edu/cs/mobile438/firstinclassactivity1/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.cs.mobile438.firstinclassactivity1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
